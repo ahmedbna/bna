2a2fee7422f5de6766f205010459d0a876a526be
ahmedbna <hi@ahmedbna.com>
2024-04-08 22:54:10 +0200

    test middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ['doting-peacock-335.convex.cloud', 'files.edgestore.dev'],
+  // },
   images: {
-    domains: ['doting-peacock-335.convex.cloud', 'files.edgestore.dev'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'doting-peacock-335.convex.cloud',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.edgestore.dev',
+        port: '',
+      },
+    ],
   },
 };
 
