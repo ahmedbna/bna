90f4e563eb522a8b1679d64025d1a952c4dd5d5b
ahmedbna <hi@ahmedbna.com>
2024-04-08 23:22:53 +0200

    meta

--- src/app/(app)/post/[id]/page.tsx
+++ src/app/(app)/post/[id]/page.tsx
@@ -13,64 +13,64 @@ type Props = {
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const token = await getAuthToken();
-//   const post = await fetchQuery(
-//     api.posts.getPostById,
-//     { postId: params.id },
-//     { token }
-//   );
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const token = await getAuthToken();
+  const post = await fetchQuery(
+    api.posts.getPostById,
+    { postId: params.id },
+    { token }
+  );
 
-//   if (!post) {
-//     return {
-//       title: 'Post!',
-//       description: 'Post is not found!',
-//     };
-//   }
+  if (!post) {
+    return {
+      title: 'Post!',
+      description: 'Post is not found!',
+    };
+  }
 
-//   const content = JSON.parse(post.content || '[]');
-//   const excerpt = content.filter(
-//     (block: Block) => block.type === 'paragraph' && block.content?.length
-//   )[0]?.content?.[0]?.text;
+  const content = JSON.parse(post.content || '[]');
+  const excerpt = content.filter(
+    (block: Block) => block.type === 'paragraph' && block.content?.length
+  )[0]?.content?.[0]?.text;
 
-//   return {
-//     title: post.title,
-//     description: excerpt,
-//     alternates: {
-//       canonical: `/package/${post.title}`,
-//     },
-//     openGraph: {
-//       title: post.title,
-//       description: excerpt,
-//       url: `${process.env.NEXT_PUBLIC_APP_URL}/${params.id}`,
-//       siteName: post.title,
-//       images: [
-//         {
-//           url: post.coverImage as URL | string,
-//           // width: image.width,
-//           // height: image.height,
-//         },
-//       ],
-//       locale: 'en_US',
-//       type: 'website',
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: post.title,
-//       description: excerpt,
-//       images: [
-//         {
-//           url: post.coverImage as URL | string,
-//           // width: image.width,
-//           // height: image.height,
-//         },
-//       ],
-//     },
-//   };
-// }
+  return {
+    title: post.title,
+    description: excerpt,
+    alternates: {
+      canonical: `/package/${post.title}`,
+    },
+    openGraph: {
+      title: post.title,
+      description: excerpt,
+      url: `${process.env.NEXT_PUBLIC_APP_URL}/${params.id}`,
+      siteName: post.title,
+      images: [
+        {
+          url: post.coverImage as URL | string,
+          // width: image.width,
+          // height: image.height,
+        },
+      ],
+      locale: 'en_US',
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: excerpt,
+      images: [
+        {
+          url: post.coverImage as URL | string,
+          // width: image.width,
+          // height: image.height,
+        },
+      ],
+    },
+  };
+}
 
 export default function Post({ params }: Props) {
   return <PostPage postId={params.id} isDraft={false} editable={false} />;
